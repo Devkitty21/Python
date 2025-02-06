aae0571169fb6fa885f89b97953371e70ccb6643
Devkitty <vieiradiogo845@gmail.com>
2025-02-06 15:10:11 +0100

    Add files via upload

---
+++ 02-reglas_convenciones.py
@@ -0,0 +1,26 @@
+# Reglas y convenciones en nombres de variables
+
+# Ejemplos de reglas estrictas
+nombre_usuario = "Juan Perez"
+# 1nombre_usuario = "Karla Gomez"
+
+# No podemos usar palabras reservadas
+
+# class = "Mi clase"
+klass = "Mi clase"
+
+# Sensible a mayusculas y minusculas
+nombre = "Diogo"
+Nombre = "Paulo"
+
+print(nombre)
+print(Nombre)
+# print(NOMBRE) esta variable no ha sido definida por lo cual nos da un error de tipo NameError
+
+# snake case
+nombre_completo = "Ricardo Esparza"
+
+# prefijos y sufijos
+
+es_casado = False
+nombre_txt = "archivo.txt"
